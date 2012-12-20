73fe449b7c11b54d24a54bfd945ca407e8676046
koen hoeymans <koenhoeymans@gmail.com>
2012-12-20 20:23:07 +0100

    ignoring more and more

--- AnyMark/Autoload.php
+++ AnyMark/Autoload.php
@@ -17,19 +17,18 @@ function AnyMark_Autoload($className)
 	if (file_exists($classNameFile))
 	{
 		require_once $classNameFile;
-		return;
-	}
-
-	$classNameFile = __DIR__
-		. DIRECTORY_SEPARATOR . '..'
-		. DIRECTORY_SEPARATOR . 'Vendor'
-		. DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className)
-		. '.php';
-
-	if (file_exists($classNameFile))
-	{
-		require_once $classNameFile;
 	}
 }
 
 spl_autoload_register('AnyMark_Autoload');
+
+if (file_exts(__DIR__
+		. DIRECTORY_SEPARATOR . '..'
+		. DIRECTORY_SEPARATOR . 'Vendor'
+		. DIRECTORY_SEPARATOR . 'Autoload.php'))
+{
+	require_once(__DIR__
+		. DIRECTORY_SEPARATOR . '..'
+		. DIRECTORY_SEPARATOR . 'Vendor'
+		. DIRECTORY_SEPARATOR . 'Autoload.php');
+}

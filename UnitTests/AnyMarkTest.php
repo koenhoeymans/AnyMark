27075c9414e4af24e57b1a750cf78cfc7b18ca8e
koen hoeymans <koenhoeymans@gmail.com>
2013-04-26 21:15:40 +0200

    improved test

--- UnitTests/AnyMarkTest.php
+++ UnitTests/AnyMarkTest.php
@@ -9,7 +9,12 @@ class AnyMark_AnyMarkTest extends PHPUnit_Framework_TestCase
 {
 	public function setup()
 	{
-		$this->anyMark = \AnyMark\AnyMark::setup();
+		$this->parser = $this->getMock('\\AnyMark\\Parser\\Parser');
+		$this->eventDispatcher = $this->getMock('\\Epa\\EventDispatcher');
+		$this->patternConfig = $this->getMock('\\AnyMark\\Pattern\\FileArrayPatternConfig');
+		$this->anyMark = new \AnyMark\AnyMark(
+			$this->parser, $this->eventDispatcher, $this->patternConfig
+		);
 	}
 
 	/**
@@ -20,9 +25,23 @@ class AnyMark_AnyMarkTest extends PHPUnit_Framework_TestCase
 		$observer = $this->getMock('\\Epa\\Observer');
 		$this->anyMark->addObserver($observer);
 
+		$this->parser
+			->expects($this->once())
+			->method('parse')
+			->will($this->returnValue(new \ElementTree\ElementTree()));
+
 		$observer
-			->expects($this->exactly(3))
-			->method('notify');
+			->expects($this->at(0))
+			->method('notify')
+			->with(new \AnyMark\Events\PatternConfigFile($this->patternConfig));
+		$observer
+			->expects($this->at(1))
+			->method('notify')
+			->with(new \AnyMark\Events\BeforeParsing("text\n\n"));
+		$observer
+			->expects($this->at(2))
+			->method('notify')
+			->with(new \AnyMark\Events\AfterParsing(new \ElementTree\ElementTree()));
 
 		$this->anyMark->parse('text');
 	}
@@ -32,6 +51,11 @@ class AnyMark_AnyMarkTest extends PHPUnit_Framework_TestCase
 	 */
 	public function returnsParsingResultAsElementTree()
 	{
+		$this->parser
+			->expects($this->once())
+			->method('parse')
+			->will($this->returnValue(new \ElementTree\ElementTree()));
+
 		$this->assertTrue(
 			$this->anyMark->parse('text') instanceof \ElementTree\ElementTree
 		);

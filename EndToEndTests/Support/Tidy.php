7033c66a8f4576da40904ebcd49707366b5ecc4a
koenhoeymans <koenhoeymans@gmail.com>
2013-12-04 07:40:00 +0100

    changed output comparison to Comparify library

--- EndToEndTests/Support/Tidy.php
+++ EndToEndTests/Support/Tidy.php
@@ -4,27 +4,15 @@ namespace AnyMark\EndToEndTests\Support;
 
 class Tidy extends \PHPUnit_Framework_TestCase
 {
+	private $comparify;
+
+	public function setup()
+	{
+		$this->comparify = new \Comparify\Comparify();
+	}
+
 	public function tidy($html)
 	{
-		// @todo normalize if tidy not available
-		// @todo set config so that nothings changed but styling
-		if (function_exists('tidy_parse_string'))
-		{
-			$config = array(
-	           'indent'         => true,
-	           'wrap'           => 200,
-	           'tab-size'		=> 4,
-	           'preserve-entities'	=> 1,
-	           'quote-ampersand'	=> 0,
-	           'quote-marks'	=> 0,
-	           'char-encoding'	=> 'utf8',
-	           'clean' => false
-			);
-
-			$tidy = tidy_parse_string($html, $config);
-			$html = tidy_get_output($tidy);
-		}
-
-		return $html;
+		return $this->comparify->transform($html);
 	}
 }
